d735b526ccf1625daf0f303ff21cf0f6e7c41077
Unknown <felix.herrenschmidt@isen.fr>
2019-06-17 20:38:43 +0200

    Added file managment in FileViewController
    Folders: Going back and forward

--- fido-eurosmart/FileViewController.swift
+++ fido-eurosmart/FileViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class FileViewController: UIViewController, UINavigationBarDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    var dirList = [DirData]()
+    var tabListDirFiles = [[DirFileData]()]// Tableau des différentes requêtes
+    var lastId = 0
+    var listDirFiles = [DirFileData]()
     let ip = "172.16.103.116"
     let port = "1987" // 1988 : https, 1987: http
     var sid = ""
-    
+
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var backButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navBar.delegate = self
-        
-        let preferences = UserDefaults.standard
-        sid = String(describing: preferences.object(forKey: "sid") ?? "")
-        print("Sid: ",sid)
+
+        initBackButton()
         
         fetchDirectories()
     }
@@ -39,6 +41,14 @@ class FileViewController: UIViewController, UINavigationBarDelegate, UITableView
     // MARK: UiNavigationBarDelegate Methods
     func position(for bar: UIBarPositioning) -> UIBarPosition {
         return UIBarPosition.topAttached
+    }
+    
+    func initBackButton(){
+        backButton.title = ""
+        // Alignement du boutton au centre
+        toolBar.sizeToFit()
+        let flexible = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        toolBar.items = [flexible,backButton,flexible]
     }
     
     func fetchDirectories() {
@@ -79,7 +89,62 @@ class FileViewController: UIViewController, UINavigationBarDelegate, UITableView
                     }
                     for json in jsonArray
                     {
-                        self.dirList.append(DirData(json))
+                        self.listDirFiles.append(DirFileData(json))
+                    }
+                    self.tabListDirFiles[0] = self.listDirFiles
+                    DispatchQueue.main.async(
+                        execute:self.fetchDone
+                    )
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    func fetchDirectoriesDetails(_ folder_path:String) {
+        backButton.title = "Back"
+        let path = folder_path.replacingOccurrences(of: " ", with: "%20") // Gestion des espaces
+        let url = URL(string: "http://\(ip):\(port)/webapi/entry.cgi?api=SYNO.FileStation.List&version=2&method=list&folder_path=\(path)&_sid=\(sid)") // À passer en https, avec cert let's encrypt
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) in
+            guard let _:Data = data else
+            {
+                return
+            }
+            
+            let json:Any?
+            
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            }
+            catch
+            {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            if let data_block = server_response["data"] as? NSDictionary
+            {
+                if let JSON = data_block as? [String: Any] {
+                    guard let jsonArray = JSON["files"] as? [[String: Any]] else {
+                        return
+                    }
+                    self.lastId+=1
+                    self.tabListDirFiles.insert(self.listDirFiles, at: self.lastId)
+                    self.listDirFiles.removeAll()
+                    for json in jsonArray
+                    {
+                        self.listDirFiles.append(DirFileData(json))
                     }
                     DispatchQueue.main.async(
                         execute:self.fetchDone
@@ -95,19 +160,37 @@ class FileViewController: UIViewController, UINavigationBarDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dirList.count
+        return listDirFiles.count
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        if(self.listDirFiles[indexPath.row].isDir == true){
+        fetchDirectoriesDetails(self.listDirFiles[indexPath.row].path)
+        }else{
+            print("It is a file")
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currentDir = dirList[indexPath.row]
+        let currentDir = listDirFiles[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
         cell.textLabel?.text = currentDir.dirName
-        cell.detailTextLabel?.text = currentDir.path
+        cell.detailTextLabel?.text = "Directory: "+String(describing:currentDir.isDir)
         return cell
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        // Si on n'est pas au dossier racine
+        if (self.lastId != 0){
+            self.listDirFiles.removeAll()
+            self.listDirFiles = self.tabListDirFiles[self.lastId]
+            self.tableView.reloadData()
+            self.lastId-=1
+            if (self.lastId == 0){
+                // On fait disparaitre le bouton de retour
+                backButton.title = ""
+            }
+        }
     }
     
     @IBAction func logout(_ sender: Any) {
@@ -117,10 +200,12 @@ class FileViewController: UIViewController, UINavigationBarDelegate, UITableView
     }
 }
 
-struct DirData {
+struct DirFileData {
+    var isDir: Bool
     var dirName: String
     var path: String
     init(_ dictionary: [String: Any]) {
+        self.isDir = dictionary["isdir"]! as! Bool
         self.dirName = dictionary["name"]! as! String
         self.path = dictionary["path"]! as! String
     }
